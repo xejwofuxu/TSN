edbb83cbb70836297e40b5a122c96a7545a0e065
xejwofuxu <1968643693@qq.com>
2022-04-29 15:57:46 +0800

    test_logger

--- test/test_logger.cc
+++ test/test_logger.cc
@@ -1,10 +1,15 @@
 #include "faker_tsn.h"
 #include "test.h"
+#include <gtest/gtest.h>
 
 using namespace faker_tsn;
 
-void test_logger() {
+void Test_Logger() {
     Log::setLoggerLevel(Log::INFO_LEVEL);
     DEBUG("debug world");
     INFO("info world");
+}
+
+TEST(TEST_LOGGER, TEST_LOGGER_READ) {
+    Test_Logger();
 }
