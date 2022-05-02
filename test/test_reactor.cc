fac8c07aaad9e1bbc08816c6c1391f6ebc5c229a
xejwofuxu <1968643693@qq.com>
2022-05-02 11:05:57 +0800

    test_reactor

--- test/test_reactor.cc
+++ test/test_reactor.cc
@@ -5,10 +5,23 @@
 using namespace faker_tsn;
 
 static void TestReactor() {
-    std::shared_ptr<IPort> port = std::make_shared<ConsolePort>();  // create port
+    // stdinEventHandler, EVENT_TYPE::READ
+    // std::shared_ptr<IPort> port = std::make_shared<ConsolePort>();  // create port
 
+    // port->registerEventHandler();
+    // Reactor::getInstance().handle_events();
+
+    // recvTSNFrameEventHandler, EVENT_TYPE::READ
+    // sendTSNFrameEventHandler, EVENT_TYPE::WRITE
+    PortManager portManager;
+    // 获取所有设备名称
+    portManager.findAllDeviceName();
+    portManager.createPortFromDeviceNameList();
+    // 获取端口号
+    auto port = portManager.getPort(0);
     port->registerEventHandler();
     Reactor::getInstance().handle_events();
+    port->sendTest();
 }
 
 TEST(TEST_REACTOR, TEST_REACTOR) {
