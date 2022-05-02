cdae0bef354a9aac20ebd259a7568dd5d9349d2d
xejwofuxu <1968643693@qq.com>
2022-05-02 11:06:16 +0800

    测试多端口

--- test/test_h2_forward.cc
+++ test/test_h2_forward.cc
@@ -7,19 +7,22 @@ using namespace faker_tsn;
 
 static void Sender() {
     // crate port 1
-    const char* name1 = "s2-eth0";
+    // const char* name1 = "s2-eth0";
+    const char* name1 = "ens33";
     std::shared_ptr<IPort> port1 = std::make_shared<DataPort>(name1);
     std::shared_ptr<IPortState> creationState1 = std::make_shared<CreationPortState>();
     creationState1->doAction(port1);
 
     // crate port 2
-    const char* name2 = "s2-eth1";
+    // const char* name2 = "s2-eth1";
+    const char* name2 = "ens33";
     std::shared_ptr<IPort> port2 = std::make_shared<DataPort>(name2);
     std::shared_ptr<IPortState> creationState2 = std::make_shared<CreationPortState>();
     creationState2->doAction(port2);
 
     // crate port 3
-    const char* name3 = "s2-eth2";
+    // const char* name3 = "s2-eth2";
+    const char* name3 = "ens33";
     std::shared_ptr<IPort> port3 = std::make_shared<DataPort>(name3);
     std::shared_ptr<IPortState> creationState3 = std::make_shared<CreationPortState>();
     creationState2->doAction(port3);
@@ -31,7 +34,7 @@ static void Sender() {
     portManager->appendPort(port3);
 
     // load mac table
-    MacTable::loadRouteXML("./config/routes.xml");
+    MacTable::loadRouteXML("/home/reptile/下载/TSN/config/routes.xml");
 
     // enable reactor
     Reactor::getInstance().handle_events();
