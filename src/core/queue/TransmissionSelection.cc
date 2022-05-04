7c1ef989f824120d34a9af517739c51855a58a9f
xejwofuxu <1968643693@qq.com>
2022-05-04 11:47:21 +0800

    严格优先级

--- src/core/queue/TransmissionSelection.cc
+++ src/core/queue/TransmissionSelection.cc
@@ -10,7 +10,7 @@ namespace faker_tsn {
 
 IFrameBody* TransmissionSelection::dequeue(QueueContext& queueContext) {
     INFO(queueContext.m_deviceName + ".port" + std::to_string(queueContext.m_portIndex) + " TransmissionSelection on call");
-    /* invoke queueu from 7 to 0 */
+    /* invoke queue from 7 to 0 */
     for (auto it = queueContext.m_queues.rbegin(); it != queueContext.m_queues.rend(); it++) {
         IFrameBody* frame = (*it)->dequeue();
         if (frame != nullptr)
